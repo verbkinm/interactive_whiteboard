1f7465122b8eb7ce04a1620800062fb6d405b2e5
verbkinm <verbkinm@yandex.ru>
2018-07-12 15:25:59 +0500

    продолжение

---
+++ convert_main_table_schedule/convert_main_table_schedule/converter_main_table_schedule.h
@@ -0,0 +1,22 @@
+#ifndef CONVERTER_MAIN_TABLE_SHEDULE_H
+#define CONVERTER_MAIN_TABLE_SHEDULE_H
+
+#include <QObject>
+#include <QFileInfo>
+
+class Converter_main_table_shedule : public QObject
+{
+    Q_OBJECT
+public:
+    Converter_main_table_shedule(QString read_file, QObject *parent = 0);
+
+private:
+    QFile fileXmlOut, fileHtmlIn;
+
+    void clearTag(QString *string, int *positionString, int indexStart, int indexEnd);
+    void removeExcess(QString *string);
+    void copyFilesInArchive();
+
+};
+
+#endif // CONVERTER_MAIN_TABLE_SHEDULE_H
